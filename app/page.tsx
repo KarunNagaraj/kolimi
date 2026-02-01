b62ebc848f6fb992a8aaf9abc3164d5b37ac8e29
KarunNagaraj <karun.nagraj@gmail.com>
2026-02-01 16:06:53 +0530

    Fix Netlify form submission capture

--- app/page.tsx
+++ app/page.tsx
@@ -164,13 +164,14 @@ export default function Home() {
           <form
             name="apartment-rent-enquiry"
             method="POST"
-            action="/thank-you"
+            action="/"
             data-netlify="true"
             netlify-honeypot="bot-field"
             className="space-y-6"
           >
             {/* Netlify hidden fields */}
             <input type="hidden" name="form-name" value="apartment-rent-enquiry" />
+            <input type="hidden" name="redirect" value="/thank-you" />
             <input type="hidden" name="bot-field" />
 
             {/* Property metadata */}
